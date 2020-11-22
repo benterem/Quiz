6b382349eac3ef3a77221e394f7993cee83f6e62
benterem <benterem@gmail.com>
2020-11-22 13:27:25 +0200

    .

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-//initrwrwqerq
+const answers = ["C", "B", "A", "C"];
+const form = document.querySelector(".quiz-form");
+const result= document.querySelector(".result")
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    
+    userAnswers.forEach((answer, index) => {
+        if(answer === answers[index]){
+            score+=(1 / answers.length) * 100;
+        }
+    });
+
+    scrollTo(0, 0);
+
+    result.classList.remove('d-none');
+    
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output == score){
+            clearInterval(timer);
+        }
+        output++;
+    }, 10);
+
+});
